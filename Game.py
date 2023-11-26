d287d5a58c465d152da18d64d77247b4d0440992
PeCMay2105 <pedrocmaia2105@gmail.com>
2023-11-26 20:06:08 -0300

    parte principal do jogo finalizada

--- Game.py
+++ Game.py
@@ -1,7 +1,7 @@
 import pygame
 from sys import exit
 from pygame.locals import *
-import math
+import random
 import tabuleiro
 import elementos
 import menu
@@ -16,8 +16,45 @@ def gera_mascara_para_o_avatar(key):
         mascara = elementos.desenha_quadrado(tela,tabuleiro.backgorund_color,posicao_avatar_X,(posicao_avatar_Y)+velocidade,largura_avatar,altura_avatar)
     else: mascara = elementos.desenha_quadrado(tela,tabuleiro.backgorund_color,posicao_avatar_X,(posicao_avatar_Y)-velocidade,largura_avatar,altura_avatar)
     return mascara
-#Variaveis globais:
+def gera_mascara_objeto(key):
+    global velocidade;
+    if key == "w":
+        mascara = elementos.desenha_quadrado(tela,tabuleiro.backgorund_color,posicao_avatar_X,(posicao_avatar_Y)+velocidade,largura_avatar,altura_avatar)
+    else: mascara = elementos.desenha_quadrado(tela,tabuleiro.backgorund_color,posicao_avatar_X,(posicao_avatar_Y)-velocidade,largura_avatar,altura_avatar)
+    return mascara
 
+def pontos_e_combustível():
+    
+    global pontos;
+    pontuacao = str(pontos)
+    global combustivel;
+    tanque_combustivel = str(combustivel)
+    global tela;
+    fonte = tabuleiro.gerador_de_fonte("Monospace",20)
+    mostrador_pontuacao = tabuleiro.gerador_de_texto(pontuacao,fonte,(255,255,255))
+    mostrador_combustivel = tabuleiro.gerador_de_texto(tanque_combustivel,tabuleiro.gerador_de_fonte("Monospace",20),(255,255,255))
+    pygame.draw.rect(tela,(0,0,0),(50,12,100,16))
+    pygame.draw.rect(tela,(0,0,0),(900,12,100,16))
+    
+    tela.blit(mostrador_pontuacao,(50,12))
+    tela.blit(mostrador_combustivel,(900,12))
+
+
+def aparicao_objetos():
+   for i in inimigos:
+        i["posx"] -= velocidade_inimigo
+
+   if random.randint(0, 10) > 2:
+        novo_inimigo = {"posx": 940, "posy": random.randint(50, altura_janela - altura_inimigo), "altura": altura_inimigo, "largura": largura_inimigo}
+        if all(elementos.distancia_minima(novo_inimigo, inimigo, distancia_minima_entre_objetos) for inimigo in inimigos):
+            inimigos.append(novo_inimigo)
+   elif random.randint(0, 10)> 8:
+        novo_tanque = {"posx": 940, "posy": random.randint(50, altura_janela - altura_inimigo), "altura": altura_avatar, "largura": largura_avatar}
+        if all(elementos.distancia_minima(novo_tanque, tanque_de_combustivel, distancia_minima_entre_objetos) for tanque_de_combustivel in tanques):
+            tanques.append(novo_tanque)
+
+#Variaveis globais:
+probabilidade_inimigo = 0
 largura_janela = 950 # largura da janela
 altura_janela  = 480 # altura da janela
 largura = 950 #largura do tabuleiro
@@ -26,14 +63,36 @@ clock = pygame.time.Clock()
 fps = 24 #valor inicial para o fps. Incrementa com o tempo
 combustivel = 600
 pontos = 0
-velocidade = 5
+velocidade = 20
+distancia_minima_entre_objetos = 100
+## Inicialização dos personagens
 
-## Inicialização do avatar
+### tanques
+tanques = []
+posicao_x_tanque = random.randrange(475,950)
+posicao_y_tanque = altura_janela/random.randrange(1,150)
+velocidade_tanque_de_combustivel = 0.5
+
+### inimigos
+inimigos = []
+posicao_inimigo_X = 940
+posicao_inimigo_Y = random.randrange(100,480)
+velocidade_inimigo = 4
+largura_inimigo = 20
+altura_inimigo = 20
+
+### avatar
 posicao_avatar_X = 20 # personagem começa no início do eixo x do tabuleiro de jogo
 posicao_avatar_Y = 50 #personagem começa na metade do eixo y do tabuleiro
-
 largura_avatar = 20
 altura_avatar = 20
+
+
+### projétil
+projetil_lista = []
+projetil = ""
+vel_projetil = 7
+
 ######################################################################
 
 #inicializadores dos elementos principais
@@ -41,7 +100,7 @@ tela = tabuleiro.gerador_de_superficie(largura_janela,altura_janela)
 state = True #condição para o loop de menu funcionar
 #loop Menu
 while state == True:
-    menu.Menu(tela,["Jogar","Configurações","Sobre","Créditos"],400,240)
+    menu.Menu(tela,["0 - Jogar","1 - Configurações","2 - Sobre","3 - Créditos"],400,240)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -54,34 +113,87 @@ while state == True:
 
 mapa = tabuleiro.gerador_de_mapa(tela,largura,altura,5)
 A = elementos.desenha_quadrado(tela,(0,250,154),posicao_avatar_X,posicao_avatar_Y,largura_avatar,altura_avatar) # (Avatar do jogo)
+retangulo_referencia_A = pygame.Rect(posicao_avatar_X,posicao_avatar_Y,largura,altura)
 gera_avatar()
 
 
 #Loop de jogo
+
 while True:
+    pontos_e_combustível()
+    for i in inimigos:
+        i["posx"] -= velocidade_inimigo
+    for i in tanques:
+        i["posx"] -= velocidade_tanque_de_combustivel
+    for i in projetil_lista:
+        i.x += vel_projetil
+    aparicao_objetos()
 
+    mapa = tabuleiro.gerador_de_mapa(tela, largura, altura, 5)
+    A = elementos.desenha_quadrado(tela, (0, 250, 154), posicao_avatar_X, posicao_avatar_Y, largura_avatar, altura_avatar)  # Avatar do jogo
+    retangulo_referencia_A = pygame.Rect(posicao_avatar_X, posicao_avatar_Y, largura_avatar, altura_avatar)
+    gera_avatar()
 
+    for inimigo in inimigos:
+        elementos.desenha_quadrado(tela,(255,0,0),inimigo["posx"],inimigo["posy"],inimigo["largura"],inimigo["altura"])
+        if elementos.verifica_colisao_entre_rects(retangulo_referencia_A, pygame.Rect(inimigo["posx"], inimigo["posy"], inimigo["largura"], inimigo["altura"])):
+                print("Colisão com inimigo")
+                # Remova ou trate a colisão conforme necessário
+
+    for tanque in tanques:
+        elementos.desenha_quadrado(tela, (0, 206, 209), tanque["posx"], tanque["posy"], largura_avatar, altura_avatar)
+        if elementos.verifica_colisao(tanque["posy"], tanque["posx"], tanque["largura"], tanque["altura"], posicao_avatar_Y, posicao_avatar_X, largura_avatar, altura_avatar):
+            combustivel += 1
+    
+    projetil_para_remocao = []
+
+    for projetil in projetil_lista:
+        
+        inimigos_para_remover = [inimigo for inimigo in inimigos if pygame.Rect(inimigo["posx"], inimigo["posy"], inimigo["altura"], inimigo["largura"]).colliderect(projetil)]
+        projetil_para_remocao.extend(inimigos_para_remover)
+        if pygame.Rect(inimigo["posx"], inimigo["posy"], inimigo["altura"], inimigo["largura"]).colliderect(projetil):
+            projetil_lista.remove(projetil)
+        pygame.draw.rect(tela, (0, 0, 255), projetil)
+
+    # Remove inimigos atingidos por projéteis
+    for inimigo in projetil_para_remocao:
+        inimigos.remove(inimigo)
+        pontos += 1
+
+    # Remove projéteis que atingiram inimigos
+    projetil_lista = [projetil for projetil in projetil_lista if projetil not in projetil_para_remocao]
+        
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
         if event.type == KEYDOWN:
             if event.key == K_w:
-                if posicao_avatar_Y > 0:
+                combustivel += 0.01
+                combustivel -= 1
+                if posicao_avatar_Y >30 :
                     posicao_avatar_Y -=velocidade
                     gera_avatar()
                     gera_mascara_para_o_avatar("w")
                     gera_avatar()
-                    
-                    pygame.display.flip()
+                    print(posicao_avatar_Y)
                 else: 
                     pass      
             elif event.key == K_s:
-                if posicao_avatar_Y < altura_janela:
+                combustivel += 0.01
+                combustivel -=1
+                if posicao_avatar_Y < 460:
                     posicao_avatar_Y += velocidade
                     gera_avatar()
                     gera_mascara_para_o_avatar("s")
                     gera_avatar()
-                    pygame.display.flip()              
+            elif event.key == K_t:
+                projetil = pygame.Rect(retangulo_referencia_A.right, retangulo_referencia_A.centery - 10, 10, 10)
+                projetil_lista.append(projetil)
+
+    pygame.display.flip()           
     pygame.display.update()
-    clock.tick()
+    clock.tick(60)
+    
+    combustivel -= 0.01
+
--- elementos.py
+++ elementos.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 def desenha_quadrado(surface,backgorund_color,posx,posy,largura,altura):
     rect =pygame.draw.rect(surface, backgorund_color, pygame.Rect(posx,posy,largura,altura))
     return rect
@@ -6,4 +8,23 @@ def desenha_quadrado(surface,backgorund_color,posx,posy,largura,altura):
 def desenha_linhas(surface,background_color,tupleStart,tupleEnd):
     line = pygame.draw.line(surface,background_color,tupleStart,tupleEnd)
 
+
+def des_objetos(surface,largura_tela,altura_tela,largura_obj,altura_obj):
+    fator_aleatorio = random()
+    posx = largura_tela*fator_aleatorio
+    posy = altura_tela*fator_aleatorio
+    desenha_quadrado(surface,(255,0,0),posy,posx,largura_obj,altura_obj)
+
+
+def distancia_minima(pos1,pos2,distancia_definida):
+    return abs(pos1["posx"] - pos2["posx"]) > distancia_definida
+
+def verifica_colisao(posy1,posx1,l1,a1,posy2,posx2,l2,a2):
+    rect1 = pygame.Rect(posx1,posy1,a1,l1)
+    rect2 = pygame.Rect(posx2,posy2,a2,l2)
+    return rect1.colliderect(rect2)
+
+def verifica_colisao_entre_rects(rect1,rect2):
+    return rect1.colliderect(rect2)
+    
 
